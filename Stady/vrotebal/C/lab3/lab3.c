6b21836b1cf25c54650c4660e757d84aed5a57d6
Steamky <oegorych97@mail.ru>
2024-02-22 16:12:14 +1000

    Все розы попадают в ад

--- Stady/vrotebal/C/lab3/lab3.c
+++ Stady/vrotebal/C/lab3/lab3.c
@@ -5,11 +5,17 @@
 
 int main()
 {
-   int stroka[1024];
-   printf("Input any word:\n");
-   scanf("%d", &stroka);
-
-   printf("%d in rework type is: %d", stroka, toupper(stroka));
-
+   char str[81];
+   printf("Insert any word: \n");
+   fgets(str,81,stdin);
+   printf("%s", str);
+   for (int i = 0; i < 80; i++)
+   {
+      if (str[i] == "a" || str[i] == "b")
+      {
+         str[i] = str[i]+32;
+      }
+   }
+   printf("%s", str);
    return 0;
 }
