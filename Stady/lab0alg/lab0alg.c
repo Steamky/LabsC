1cc611e2f7328cb561b7e268f394a6798a981ed0
Steamky <oegorych97@mail.ru>
2024-04-12 13:54:49 +1000

    И снова обогнал, снова обогнал...

---
+++ Stady/lab0alg/lab0alg.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct point
+{
+    double x;
+    double y;
+} POINT;
+
+typedef struct poly
+{
+    int n;
+    POINT *p;
+} POLY;
+
+double dist(POINT p1, POINT p2)
+{
+    return sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+}
+
+double perimeter(POLY p)
+{
+    double s = 0;
+    for(int i = 0; i < p.n; i++)
+    {
+        s += dist(p.p[i], p.p[(i + 1) % p.n]);
+    }
+    return s;
+}
+
+double area(POLY p)
+{
+    double s = 0;
+    for(int i = 0; i < p.n; i++)
+    {
+        s += dist(p.p[(i + 1) % p.n], p.p[i]);
+    }
+    return s;
+}
+
+double diameter(POLY p)
+{
+    double max = 0;
+    for (int i = 0; i < p.n; i++)
+    {
+        for (int j = i + 1; j < p.n; j++)
+        {
+            double d = dist(p.p[i], p.p[j]);
+            if (d > max)
+            {
+                max = d;
+            }
+        }
+    }
+    return max;
+}
+
+int main(int argc, char *argv[])
+{
+    FILE *F;
+    POLY p;
+    
+    F = fopen(argv[1], "r");
+    
+    fscanf(F, "%d", &p.n);
+    p.p = malloc(p.n * sizeof(POINT));
+    for(int i = 0; i < p.n; i++)
+    {
+        fscanf(F, "%lf %lf", &p.p[i].x, &p.p[i].y);
+    }
+    
+    printf("Периметр равен: %lf\n", perimeter(p));
+    printf("Площадь равна: %lf\n", area(p));
+    printf("Диаметр равен: %lf\n", diameter(p));
+    
+    free(p.p);
+    fclose(F);
+    
+    return 0;
+}
+
+
--- Stady/lab2alg/lab2alg.c
+++ Stady/lab2alg/lab2alg.c
@@ -1,43 +1,148 @@
+// Найти все области, статистику глифов:
+// количество черных пикселей, связность, диаметр наибольшей области
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+#define DEBUG 3
+// #undef DEBUG
+#define ISBIT(n, x) (((01 << (n)) & (x)) ? 1 : 0)
 
-double trapezoidal_rule(double z, double eps) {
-    int n = 1;
-    double prev_result = 0;
-    
-    while (1) {
-        double h = z / n;
-        double result = 0;
-        
-        for (int i = 1; i < n; i++) {
-            double x = i * h;
-            result += sinh(x) * sin(x);
-        }
-        
-        result += (sinh(0) * sin(0) + sinh(z) * sin(z)) / 2;
-        result *= h;
-        
-        if (fabs(result - prev_result) < eps) {
-            return result;
-        }
-        
-        prev_result = result;
-        n *= 2;
-    }
+void outbyte(char byte)
+{ // Вывод байта в двоичном виде
+  int i;
+  for (i = 7; i > -1; i--)
+    putchar(ISBIT((i), byte) ? '*' : ' ');
+  fflush(stdout);
 }
 
-int main() {
-    double start = 0;
-    double end = 10;
-    double step = 0.2;
-    double eps = 1e-7;
-    
-    printf("Z\t\tIntegral\n");
-    printf("------------------------------\n");
-    for (double z = start; z <= end; z += step) {
-        double integral = trapezoidal_rule(z, eps);
-        printf("%f\t%f\n", z, integral);
-    }
-    
+void outbytes(int n, char *byte)
+{ // Вывод массива байтов в двоичном виде
+  int i;
+  for (i = 0; i < n; i++)
+    outbyte(byte[i]);
+  putchar('\n');
+}
+
+typedef struct img
+{ //  структура глифа - картинки
+  int w;
+  int h;     // ширина и высота в пикслелях
+  int dx;    // расстояние до следующего глифа (если буква)
+  int count; // черных пикселей всего
+  int id;    // иднтификатор - "номер" глифа
+  int bytes; // /количество байтов в битовой карте
+  double density;
+  int diam;
+  int perim;
+  int conn;
+  unsigned char *data; //  битовая карта (неупакованная)
+} IMG;
+
+int popcnt8(unsigned char i)
+{
+  int count;
+  count = 0;
+  while (i)
+  {
+    ++count;
+    i = (i - 1) & i;
+  }
+  return count;
+}
+
+IMG *init_img(int id, int w, int h)
+{ // создание пустого (чистого) глифа с заданными размерами
+  IMG *t;
+  t = (IMG *)malloc(sizeof(IMG));
+  t->w = w;
+  t->h = h;
+  t->dx = 0;
+  t->count = 0;
+  t->id = id;
+  t->bytes = (((w + 7) / 8) * h);
+  t->data = (unsigned char *)calloc(t->bytes, 1);
+  return t;
+}
+
+IMG *load_img(int id, char *s)
+{
+  FILE *F; // считывание из файла глифа с номером id
+  IMG *I;
+  I = (IMG *)malloc(sizeof(IMG));
+  F = fopen(s, "rb");
+  fread(&(I->w), sizeof(int), 1, F);
+  fread(&(I->h), sizeof(int), 1, F);
+  fread(&(I->dx), sizeof(int), 1, F);
+  fread(&(I->count), sizeof(int), 1, F);
+  fread(&(I->id), sizeof(int), 1, F);
+  fread(&(I->bytes), sizeof(int), 1, F);
+  I->data = (unsigned char *)calloc(I->bytes, 1);
+  fread(I->data, 1, I->bytes, F);
+  fclose(F);
+  return I;
+}
+
+int xcomp(IMG **a, IMG **b)
+{
+  return -(*a)->w + (*b)->w;
+}
+
+int ycomp(IMG **a, IMG **b)
+{
+  return -(*a)->h + (*b)->h;
+}
+
+IMG *G[50000];
+
+int N;
+
+int ISPIX(int argc, int i, int j)
+{
+  if (i < 0)
     return 0;
+  if (j < 0)
+    return 0;
+  if (i >= G[argc]->w)
+    return 0;
+  if (j >= G[argc]->h)
+    return 0;
+  return ISBIT((7 - j % 8), G[argc]->data[i * (G[argc]->bytes / G[argc]->h) + j / 8]);
+}
+
+int main(int argc, char *argv[])
+{
+  long int c;
+  int len;
+  unsigned char *s;
+  unsigned long long *t;
+  N = argc - 1;
+  while (argc-- > 1)
+  {
+    G[argc] = load_img(argc, argv[argc]);
+    printf("Loaded %s\n", argv[argc]);
+
+    c = 0;
+    t = (unsigned long long *)G[argc]->data;
+    s = (unsigned char *)G[argc]->data;
+
+    len = G[argc]->bytes;
+    for (int i = 0; i < G[argc]->h; i++)
+    {
+#ifdef DEBUG
+      printf("%d %d \n", G[argc]->w, G[argc]->h);
+      fflush(stdout);
+#endif
+
+      outbytes((G[argc]->w + 7) / 8, &G[argc]->data[i * (G[argc]->w + 7) / 8]);
+    }
+    for (int i = (len / 8) * 8; i < len; i++)
+    {
+      c += popcnt8(s[i]);
+#ifdef DEBUG
+      printf("COUNT8=%d %d\n", c, i);
+      fflush(stdout);
+#endif
+    }
+    G[argc]->count = c;
+  }
+  printf("%d %d\n", N, c);
 }
