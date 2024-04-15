3e74274f83be0b2ed7c243b1677384f798d4b5ee
Steamky <oegorych97@mail.ru>
2024-04-15 15:32:11 +1000

    Бросил курить и...Снова начал.

--- Stady/lab4alg/lab4alg.c
+++ Stady/lab4alg/lab4alg.c
@@ -2,160 +2,202 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_ROWS 100
-#define MAX_COLS 100
+#define MAX_ROWS 1000
+#define MAX_COLS 1000
 
-char image[MAX_ROWS][MAX_COLS];
-int rows = 0, cols = 0;
+int n, m;
+int N[100], M[100];
+char ***pic; // Заменил объявление pic на тройной указатель
 
-// Функция для ввода картинки из файла
-void input(const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        printf("Error opening file.\n");
-        return;
+// Функция для ввода "картинки" из файла в i-й массив
+void input(int cur, char *filename) {
+    FILE *F;
+    char str[512], Pic[512][512];
+    int i, len;
+    len = 0;
+    m = 0;
+    F = fopen(filename, "r");
+    while (fgets(str, 512, F)) {
+        n = strlen(str);
+        if (str[n - 1] == '\n') n--;
+        len = n;
+        strncpy(Pic[m], str, n);
+        m++;
     }
-
-    char buffer[MAX_COLS];
-    while (fgets(buffer, MAX_COLS, file) != NULL) {
-        int len = strlen(buffer);
-        if (len > 0 && buffer[len - 1] == '\n') {
-            buffer[len - 1] = '\0'; // Удаляем символ новой строки
-            len--;
-        }
-        if (len > cols) cols = len;
-        strcpy(image[rows], buffer);
-        rows++;
+    pic[cur] = (char ***)malloc(m * sizeof(char **)); // Изменение типа на char ***
+    for (i = 0; i < m; i++) {
+        pic[cur][i] = (char **)malloc(n * sizeof(char *)); // Изменение типа на char *
+        pic[cur][i][n] = '\0'; // Добавление завершающего нуля
+        strncpy(pic[cur][i], Pic[i], n);
     }
-
-    fclose(file);
+    N[cur] = n;
+    M[cur] = m;
+    printf("m=%d n=%d\n", m, n);
 }
 
-// Функция для вывода картинки
-void output() {
-    for (int r = 0; r < rows; r++) {
-        printf("%s\n", image[r]);
+// Функция для вывода "картинки"
+void output(int num) {
+    int n, m, i, j;
+    printf(" Picture # %d\n", num);
+    m = M[num];
+    n = N[num];
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++)
+            putchar(pic[num][i][j]);
+        putchar('\n');
     }
 }
 
 // Функция для очистки картинки (заполнить пробелами)
-void clean() {
-    for (int r = 0; r < rows; r++) {
-        for (int c = 0; c < cols; c++) {
-            image[r][c] = ' ';
+void clean(int i) {
+    for (int row = 0; row < M[i]; row++) { // Исправление на M[i]
+        for (int col = 0; col < N[i]; col++) { // Исправление на N[i]
+            pic[i][row][col] = ' ';
         }
     }
 }
 
 // Функция для инверсии картинки
-void inverse() {
-    for (int r = 0; r < rows; r++) {
-        for (int c = 0; c < cols; c++) {
-            if (image[r][c] == '*')
-                image[r][c] = ' ';
-            else
-                image[r][c] = '*';
+void inverse(int i) {
+    for (int row = 0; row < M[i]; row++) { // Исправление на M[i]
+        for (int col = 0; col < N[i]; col++) { // Исправление на N[i]
+            if (pic[i][row][col] == ' ') {
+                pic[i][row][col] = '*';
+            } else {
+                pic[i][row][col] = ' ';
+            }
         }
     }
 }
 
 // Функция для поворота картинки по часовой стрелке
-void rotate_r() {
-    char temp[MAX_ROWS][MAX_COLS];
-    for (int r = 0; r < rows; r++) {
-        for (int c = 0; c < cols; c++) {
-            temp[c][rows - 1 - r] = image[r][c];
+void rotate_r(int i) {
+    char **temp = (char **)malloc(N[i] * sizeof(char *)); // Исправление на N[i]
+    for (int row = 0; row < N[i]; row++) { // Исправление на N[i]
+        temp[row] = (char *)malloc(M[i] + 1); // Исправление на M[i]
+        temp[row][M[i]] = '\0'; // Добавление завершающего нуля
+        for (int col = 0; col < M[i]; col++) { // Исправление на M[i]
+            temp[row][col] = pic[i][M[i] - 1 - col][row]; // Исправление на M[i]
         }
     }
-    int temp_var = rows;
-    rows = cols;
-    cols = temp_var;
-    memcpy(image, temp, sizeof(temp));
+    for (int row = 0; row < N[i]; row++) { // Исправление на N[i]
+        for (int col = 0; col < M[i]; col++) { // Исправление на M[i]
+            pic[i][row][col] = temp[row][col];
+        }
+    }
+    int temp_dim = M[i];
+    M[i] = N[i];
+    N[i] = temp_dim;
+    for (int row = 0; row < N[i]; row++) { // Исправление на N[i]
+        free(temp[row]);
+    }
+    free(temp);
 }
 
 // Функция для поворота картинки против часовой стрелки
-void rotate_l() {
-    char temp[MAX_ROWS][MAX_COLS];
-    for (int r = 0; r < rows; r++) {
-        for (int c = 0; c < cols; c++) {
-            temp[rows - 1 - c][r] = image[r][c];
+void rotate_l(int i) {
+    char **temp = (char **)malloc(N[i] * sizeof(char *)); // Исправление на N[i]
+    for (int row = 0; row < N[i]; row++) { // Исправление на N[i]
+        temp[row] = (char *)malloc(M[i] + 1); // Исправление на M[i]
+        temp[row][M[i]] = '\0'; // Добавление завершающего нуля
+        for (int col = 0; col < M[i]; col++) { // Исправление на M[i]
+            temp[row][col] = pic[i][col][N[i] - 1 - row]; // Исправление на N[i]
         }
     }
-    int temp_var = rows;
-    rows = cols;
-    cols = temp_var;
-    memcpy(image, temp, sizeof(temp));
+    for (int row = 0; row < N[i]; row++) { // Исправление на N[i]
+        for (int col = 0; col < M[i]; col++) { // Исправление на M[i]
+            pic[i][row][col] = temp[row][col];
+        }
+    }
+    int temp_dim = M[i];
+    M[i] = N[i];
+    N[i] = temp_dim;
+    for (int row = 0; row < N[i]; row++) { // Исправление на N[i]
+        free(temp[row]);
+    }
+    free(temp);
 }
 
 // Функция для создания рамки из символов c
-void frame(char c) {
-    for (int r = 0; r < rows; r++) {
-        for (int c = 0; c < cols; c++) {
-            if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1) {
-                image[r][c] = c;
+void frame(int i, char c) {
+    for (int row = 0; row < M[i]; row++) { // Исправление на M[i]
+        for (int col = 0; col < N[i]; col++) { // Исправление на N[i]
+            if (row == 0 || row == M[i] - 1 || col == 0 || col == N[i] - 1) { // Исправление на M[i] и N[i]
+                pic[i][row][col] = c;
             }
         }
     }
 }
 
 // Функция для заполнения пробелов в картинке точками
-void dotting() {
-    for (int r = 0; r < rows; r++) {
-        for (int c = 0; c < cols; c++) {
-            if (image[r][c] == ' ')
-                image[r][c] = '.';
+void dotting(int i) {
+    for (int row = 0; row < M[i]; row++) { // Исправление на M[i]
+        for (int col = 0; col < N[i]; col++) { // Исправление на N[i]
+            if (pic[i][row][col] == ' ') {
+                pic[i][row][col] = '.';
+            }
         }
     }
 }
 
 // Функция для сохранения картинки в файл
-void save(const char *filename) {
+void save(int i, char *filename) {
     FILE *file = fopen(filename, "w");
     if (file == NULL) {
-        printf("Error opening file.\n");
-        return;
+        printf("Ошибка при создании файла для сохранения изображения!\n");
+        exit(1);
     }
 
-    for (int r = 0; r < rows; r++) {
-        fprintf(file, "%s\n", image[r]);
+    for (int row = 0; row < M[i]; row++) { // Исправление на M[i]
+        fprintf(file, "%s\n", pic[i][row]);
     }
 
     fclose(file);
 }
 
 int main() {
-    const char filename[] = "pic1.txt";
-    input(filename);
+    char filename[256] = "pic1.txt";
 
-    printf("Original image:\n");
-    output();
+    pic = (char ***)malloc(100 * sizeof(char **)); // Изменение типа на char *** и выделение памяти
 
-    clean();
-    printf("\nCleaned image:\n");
-    output();
+    for (int i = 0; i < 100; i++) pic[i] = NULL;
 
-    inverse();
-    printf("\nInversed image:\n");
-    output();
+    input(0, filename);
+    output(0);
 
-    rotate_r();
-    printf("\nRotated clockwise image:\n");
-    output();
+    clean(0);
+    printf("\nКартинка после очистки:\n");
+    output(0);
 
-    rotate_l();
-    printf("\nRotated counterclockwise image:\n");
-    output();
+    inverse(0);
+    printf("\nКартинка после инверсии:\n");
+    output(0);
 
-    frame('*');
-    printf("\nFramed image:\n");
-    output();
+    rotate_r(0);
+    printf("\nКартинка после поворота по часовой стрелке:\n");
+    output(0);
 
-    dotting();
-    printf("\nDotted image:\n");
-    output();
+    frame(0, '#');
+    printf("\nКартинка с рамкой:\n");
+    output(0);
 
-    save("output.txt");
-    printf("\nSaved image to output.txt\n");
+    dotting(0);
+    printf("\nКартинка с точками:\n");
+    output(0);
+
+    save(0, "output.txt");
+    printf("\nКартинка сохранена в файле output.txt\n");
+
+    // Освобождение памяти
+    for (int i = 0; i < 100; i++) {
+        if (pic[i] != NULL) {
+            for (int j = 0; j < M[i]; j++) {
+                free(pic[i][j]);
+            }
+            free(pic[i]);
+        }
+    }
+    free(pic);
 
     return 0;
 }
